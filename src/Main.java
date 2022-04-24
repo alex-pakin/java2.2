b7de926a6a8f02551b72d0808b84b7b5aaccbd0c
alex-pakin <aleksei_pakin@mail.ru>
2022-04-24 22:23:25 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initial_amount = 25;
+        int refill = 50000;
+        int limit = 1000;
+
+        int bonus;
+        if (refill >= limit) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        int sum = initial_amount + refill + bonus;
+
+        System.out.println("Общая сумма на вашем счёте = " + sum);
+        System.out.println("Из них начислено бонусом = " + bonus);
+
+
+        }
+
+
+    }
